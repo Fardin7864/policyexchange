5c5dd85ba292fd5b59523af654d3bd9c296f886a
Fardin7864 <fardinhasan1888@gmail.com>
2024-04-04 16:37:09 +0600

    changes on media section

--- src/app/media/Pictures.jsx
+++ src/app/media/Pictures.jsx
@@ -5,30 +5,32 @@ import { useEffect, useState } from "react";
 
 const Pictures = () => {
   const [pictures, setPictures] = useState();
-  const [currentImg, setCurrentImg] = useState()
+  const [currentImg, setCurrentImg] = useState("/publications/2.png")
 
   useEffect(() => {
-    fetch("/team.json")
+    fetch("/pictures.json")
       .then((res) => res.json())
-      .then((data) => setPictures(data.team_members));
+      .then((data) => setPictures(data));
   }, []);
 //   console.log(pictures)
+console.log(currentImg)
   return (
     <div className=" grid grid-cols-3 grid-rows-2">
-      <div className=" border-2 border-black">
-        <Image
-        src={"/publications/1.png"}
-        width={600}
-        height={600}
-        alt="galary"
-        />
+      <div className="  border-black row-span-1">
+        <img src={currentImg} alt=""/>
       </div>
-      <div className=" col-span-2 border-2 border-black">
+      <div className=" col-span-2  border-black">
+        <div className=" grid grid-cols-6 gap-3">
 
+      {
+            pictures?.map((picture) => 
+            <img key={picture.id} onClick={() => setCurrentImg(picture.img)} src={picture.img} className=" w-40 h-40"/>)
+        }
+        </div>
       </div>
-      <div className=" col-span-3 border-2 border-black h-24">
+      {/* <div className=" col-span-3  border-black h-24">
 
-      </div>
+      </div> */}
     </div>
   );
 };
