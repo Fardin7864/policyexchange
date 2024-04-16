b68840653ddb61a5b698e9a26f02a57be572e4e1
Fardin7864 <fardinhasan1888@gmail.com>
2024-04-16 19:13:30 +0600

    update on achivmant banner

--- src/components/AchivmentBoard.jsx
+++ src/components/AchivmentBoard.jsx
@@ -19,16 +19,18 @@ const AchivmentBoard = () => {
     const formatValue = (value) => value.toFixed(0);
   return (
     <Container className=" mb-28">
-      <img
+      {/* <img
         src="/star.png"
         className="w-[300px] h-[320px] absolute z-50 top-[4665px]"
-      />
+      /> */}
       <div>
-        <Container className=" bg-[#00263a] w-full h-72 rounded-3xl overflow-hidden flex justify-center items-center">
-          <div className=" w-1/5 relative -top-2">
-            <MdChevronRight className=" text-[#a790d5] text-[90rem] font-extrabold hidden md:block relative -left-[745px]" />
+        <Container className=" px-0 bg-[#00263a] w-full h-72 rounded-3xl overflow-hidden flex justify-center items-center">
+          <div className=" relative w-full h-full ">
+            {/* <MdChevronRight className=" text-[#a790d5] text-[90rem] font-extrabold hidden md:block relative -left-[745px]" /> */}
+          <img src={"/star.png"} alt="" 
+          className="w-[800px] h-full" />
           </div>
-          <div className=" relative flex justify-between w-[4000px] px-8">
+          <div className=" relative flex justify-between w-[4000px] px-8 -left-16">
             <div>
               <VisibilitySensor onChange={handleVisibilityChange}>
                 <p className=" text-[#a790d5] text-[90px] font-bold my-4 text-center leading-9">
